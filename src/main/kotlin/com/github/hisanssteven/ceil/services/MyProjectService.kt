47db935ae1ddfeab327cae1102c5b8e2eb628454
GitHub Action <action@github.com>
2026-07-25 13:06:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/hisanssteven/ceil/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hisanssteven/ceil/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.hisanssteven.ceil.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hisanssteven.ceil.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
